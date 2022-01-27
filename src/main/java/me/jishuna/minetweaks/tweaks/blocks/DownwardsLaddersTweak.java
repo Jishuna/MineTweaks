171f12d78cf3fc7e676788ae8d9597ab4ef64fd6
coll1234567 <joshl5324@gmail.com>
2022-01-27 08:28:10 -0500

    Fix formatting and increase default max distance

--- src/main/java/me/jishuna/minetweaks/tweaks/blocks/DownwardsLaddersTweak.java
+++ src/main/java/me/jishuna/minetweaks/tweaks/blocks/DownwardsLaddersTweak.java
@@ -5,7 +5,6 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Directional;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.Event.Result;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -19,49 +18,50 @@ import me.jishuna.minetweaks.nms.NMSManager;
 
 @RegisterTweak(name = "downwards_ladders")
 public class DownwardsLaddersTweak extends Tweak {
-    private int maxDistance;
+	private int maxDistance;
 
-    public DownwardsLaddersTweak(JavaPlugin plugin, String name) {
-        super(plugin, name);
+	public DownwardsLaddersTweak(JavaPlugin plugin, String name) {
+		super(plugin, name);
 
-        addEventHandler(PlayerInteractEvent.class, this::onInteract);
-    }
+		addEventHandler(PlayerInteractEvent.class, this::onInteract);
+	}
 
-    @Override
-    public void reload() {
-        FileUtils.loadResource(getOwningPlugin(), "Tweaks/Blocks/" + this.getName() + ".yml").ifPresent(config -> {
-            loadDefaults(config, true);
+	@Override
+	public void reload() {
+		FileUtils.loadResource(getOwningPlugin(), "Tweaks/Blocks/" + this.getName() + ".yml").ifPresent(config -> {
+			loadDefaults(config, true);
 
-            this.maxDistance = config.getInt("ladder-place-distance", 5);
-        });
-    }
+			this.maxDistance = config.getInt("ladder-place-distance", 64);
+		});
+	}
 
-    private void onInteract(PlayerInteractEvent event) {
-        if (event.useInteractedBlock() == Result.DENY || event.getAction() != Action.RIGHT_CLICK_BLOCK)
-            return;
+	private void onInteract(PlayerInteractEvent event) {
+		if (event.useInteractedBlock() == Result.DENY || event.getAction() != Action.RIGHT_CLICK_BLOCK)
+			return;
 
-        Block block = event.getClickedBlock();
-        ItemStack item = event.getItem();
+		Block block = event.getClickedBlock();
+		ItemStack item = event.getItem();
 
-        if (item == null)
-            return;
+		if (item == null)
+			return;
 
-        if (block.getType() == Material.LADDER && item.getType() == Material.LADDER) {
-            Block target = block;
-            Directional current = (Directional) block.getBlockData();
+		if (block.getType() == Material.LADDER && item.getType() == Material.LADDER) {
+			Block target = block;
+			Directional current = (Directional) block.getBlockData();
 
-            for (int distance = 0; target.getY() > block.getWorld().getMinHeight() && target.getType() == Material.LADDER && distance < this.maxDistance; distance++) {
-                target = target.getRelative(BlockFace.DOWN);
-            }
+			for (int distance = 0; target.getY() > block.getWorld().getMinHeight()
+					&& target.getType() == Material.LADDER && distance < this.maxDistance; distance++) {
+				target = target.getRelative(BlockFace.DOWN);
+			}
 
-            if (target.getType().isAir() && NMSManager.getAdapter().canPlace(current, target.getLocation())) {
-                Directional ladder = (Directional) Material.LADDER.createBlockData();
-                ladder.setFacing(current.getFacing());
-                target.setBlockData(ladder);
+			if (target.getType().isAir() && NMSManager.getAdapter().canPlace(current, target.getLocation())) {
+				Directional ladder = (Directional) Material.LADDER.createBlockData();
+				ladder.setFacing(current.getFacing());
+				target.setBlockData(ladder);
 
-                if (event.getPlayer().getGameMode() != GameMode.CREATIVE)
-                    item.setAmount(item.getAmount() - 1);
-            }
-        }
-    }
+				if (event.getPlayer().getGameMode() != GameMode.CREATIVE)
+					item.setAmount(item.getAmount() - 1);
+			}
+		}
+	}
 }

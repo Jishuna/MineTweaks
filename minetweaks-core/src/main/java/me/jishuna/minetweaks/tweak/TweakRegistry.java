a6a3c6c1ef4ddbe9fd6fe1b5476ec9babcb5840f
Jishuna <joshl5324@gmail.com>
2024-01-11 14:02:15 -0500

    Automatic registering

--- minetweaks-core/src/main/java/me/jishuna/minetweaks/tweak/TweakRegistry.java
+++ minetweaks-core/src/main/java/me/jishuna/minetweaks/tweak/TweakRegistry.java
@@ -9,33 +9,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 import org.bukkit.event.Event;
+import me.jishuna.jishlib.ClassScanner;
 import me.jishuna.jishlib.datastructure.Registry;
 import me.jishuna.minetweaks.MineTweaks;
-import me.jishuna.minetweaks.tweak.armorstand.ArmorStandArmsTweak;
-import me.jishuna.minetweaks.tweak.block.BeehiveDisplayTweak;
-import me.jishuna.minetweaks.tweak.block.FastLeafDecayTweak;
-import me.jishuna.minetweaks.tweak.block.OpenThroughItemFramesTweak;
-import me.jishuna.minetweaks.tweak.block.OpenThroughSignsTweak;
-import me.jishuna.minetweaks.tweak.block.PaintingSelectorTweak;
-import me.jishuna.minetweaks.tweak.block.QuickShulkerBoxCreationTweak;
-import me.jishuna.minetweaks.tweak.block.StickyPistonConversionTweak;
-import me.jishuna.minetweaks.tweak.crafting.CustomCraftingRecipesTweak;
-import me.jishuna.minetweaks.tweak.crafting.UnlockAllRecipesTweak;
-import me.jishuna.minetweaks.tweak.dispenser.DispenserPlaceBlocksTweak;
-import me.jishuna.minetweaks.tweak.farming.CactusBonemealTweak;
-import me.jishuna.minetweaks.tweak.farming.RightClickHarvestTweak;
-import me.jishuna.minetweaks.tweak.farming.SandBonemealTweak;
-import me.jishuna.minetweaks.tweak.farming.SmallFlowerBonemealTweak;
-import me.jishuna.minetweaks.tweak.farming.SugarcaneBonemealTweak;
-import me.jishuna.minetweaks.tweak.item.ElytraTakeoffTweak;
-import me.jishuna.minetweaks.tweak.item.InfiniteWaterBucketTweak;
-import me.jishuna.minetweaks.tweak.item.TorchArrowTweak;
-import me.jishuna.minetweaks.tweak.mob.CreeperGriefingTweak;
-import me.jishuna.minetweaks.tweak.mob.EndermanGriefingTweak;
-import me.jishuna.minetweaks.tweak.mob.HorseStatsTweak;
-import me.jishuna.minetweaks.tweak.mob.PoisonPotatoBabyMobsTweak;
-import me.jishuna.minetweaks.tweak.mob.UnsaddlePigTweak;
 
 public class TweakRegistry extends Registry<String, Tweak> {
     private final Map<Class<? extends Event>, List<Tweak>> tweaksByEvent = new HashMap<>();
@@ -43,30 +21,8 @@ public class TweakRegistry extends Registry<String, Tweak> {
     private final Set<TickingTweak> tickingTweaks = new HashSet<>();
 
     public TweakRegistry() {
-        register(new ArmorStandArmsTweak());
-        register(new CustomCraftingRecipesTweak());
-        register(new UnsaddlePigTweak());
-        register(new UnlockAllRecipesTweak());
-        register(new StickyPistonConversionTweak());
-        register(new PaintingSelectorTweak());
-        register(new BeehiveDisplayTweak());
-        register(new InfiniteWaterBucketTweak());
-        register(new EndermanGriefingTweak());
-        register(new PoisonPotatoBabyMobsTweak());
-        register(new HorseStatsTweak());
-        register(new FastLeafDecayTweak());
-        register(new QuickShulkerBoxCreationTweak());
-        register(new OpenThroughSignsTweak());
-        register(new OpenThroughItemFramesTweak());
-        register(new CactusBonemealTweak());
-        register(new SugarcaneBonemealTweak());
-        register(new DispenserPlaceBlocksTweak());
-        register(new TorchArrowTweak());
-        register(new RightClickHarvestTweak());
-        register(new ElytraTakeoffTweak());
-        register(new CreeperGriefingTweak());
-        register(new SmallFlowerBonemealTweak());
-        register(new SandBonemealTweak());
+        new ClassScanner<>(this.getClass().getClassLoader(), Tweak.class, RegisterTweak.class)
+                .forEach((Consumer<Tweak>) this::register);
 
         reload();
     }
